e8f57a5c9285abae7b5856534f40702c06614181
von-ai <kuhhakuzuhdi@gmail.com>
2025-09-18 22:04:37 +0800

    Enhance Dasbor component with form submission and reset functionality; update Button component to accept onClick prop

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Button = ({ className, label }) => {
+const Button = ({ className, label, onClick }) => {
   return (
     <>
       <button
         className={`bg-indigo-500 py-2.5 px-4 text-white font-medium rounded-[5px] hover:bg-indigo-700 transition-colors ease-in-out duration-200 ${className}`}
-        type="submit"
+        onClick={onClick}
       >
         {label}
       </button>
